335e47636c7485886606d856b4d6c1342f148e58
Глеб Лысов <barabashka.zzz@gmail.com>
2017-05-22 19:58:28 +0300

    более менее что то

--- technocached/technocached.pl
+++ technocached/technocached.pl
@@ -15,23 +15,77 @@ tcp_server $listen, $port, sub {
 	my $fh = shift;
 	my $h = AnyEvent::Handle->new(
 		fh => $fh,
+		timeout => 30,
 	);
 
 	$h->on_error(sub { $h->destroy; });
 	$h->push_read( line => sub {
-		if ($_[1] =~ m/^set (?<key>)/){
-
+		if ($_[1] =~ m/^set (?<key>\w+) (?<flags>\w+) (?<expire>\w+) (?<bytes>\d+)\\r\\n/){
+			my $key = $+{key};
+			my $struct = {	flags => $+{flags},
+							expire => $+{expire},
+							bytes => $+{bytes}};
+			$_[0]->unshift_read (line => sub {
+				if ($_[1] =~ m/(?<data>.*)\\r\\n/) {
+					$struct->{data} = $+{data};
+					$db{$key} = $struct;
+					$h->push_write("STORED\r\n");
+				}
+			});
 		}
-		elsif ($_[1] =~ m/^get /) {
-
+		elsif ($_[1] =~ m/^add (?<key>\w+) (?<flags>\w+) (?<expire>\w+) (?<bytes>\d+)\\r\\n/) {
+			unless ($db{$+{key}}) {
+				my $key = $+{key};
+				my $struct = {	flags => $+{flags},
+								expire => $+{expire},
+								bytes => $+{bytes}};
+				$_[0]->unshift_read (line => sub {
+					if ($_[1] =~ m/(?<data>.*)\\r\\n/) {
+						$struct->{data} = $+{data};
+						$db{$key} = $struct;
+						$h->push_write("STORED\r\n");
+					}
+				});
+			}
+			else {
+				$h->push_write("NOT_STORED\r\n");
+			}
 		}
-		elsif ($_[1] =~ m/^add /) {
-
+		elsif ($_[1] =~ m/^replace (?<key>\w+) (?<flags>\w+) (?<expire>\w+) (?<bytes>\d+)\\r\\n/) {
+			if ($db{$+{key}}) {
+				my $key = $+{key};
+				my $struct = {	flags => $+{flags},
+								expire => $+{expire},
+								bytes => $+{bytes}};
+				$_[0]->unshift_read (line => sub {
+					if ($_[1] =~ m/(?<data>.*)\\r\\n/) {
+						$struct->{data} = $+{data};
+						$db{$key} = $struct;
+						$h->push_write("STORED\r\n");
+					}
+				});
+			}
+			else {
+				$h->push_write("NOT_STORED\r\n");
+			}
 		}
-		elsif ($_[1] =~ m/^replace /) {
-
+		elsif ($_[1] =~ m/^get (?<key>\w+)\\r\\n/) {
+			if ($db{$+{key}}) {
+				$h->push_write("VALUE ".$+{key}." ".$db{$+{key}}->{flags}." ".$db{$+{key}}->{bytes}."\r\n");
+				$h->push_write($db{$+{key}}->{data}."\r\n")
+			}
+			$h->push_write("END\r\n");
 		}
-		$h->push_write($_[1]."\n");
+		elsif ($_[1] =~ m/^delete (?<key>\w+)\\r\\n/) {
+			if ($db{$+{key}}) {
+				delete $db{$+{key}};
+				$h->push_write("DELETED\r\n");
+			}
+			else {
+				$h->push_write("NOT_FOUND\r\n");
+			}
+		}
+		$h->destroy;
 	});
 	return;
 };
